298e8dc4166d968ac8b11e0134c55fd84f573926
Francisco González <frgonzalez.415@gmail.com>
2024-05-20 16:38:22 -0300

    Se cambian rutas de imagenes

--- scripts/categoria/vidriera.js
+++ scripts/categoria/vidriera.js
@@ -7,6 +7,6 @@ const juegos = document.querySelectorAll( 'main .vidriera .juego' )
 juegos.forEach( ( juego, i ) => {
     let nombreDelJuego = juegos[i].querySelector( '.nombre' ).textContent.toLowerCase().replace( /[.]/g, '' ).replace( /[ ]/g, '_' )
     console.log(nombreDelJuego)
-    juegos[i].style.backgroundImage = `url(/imagenes/juegos/${nombreDelJuego}.png)`
+    juegos[i].style.backgroundImage = `url(../imagenes/juegos/${nombreDelJuego}.png)`
     })
 
