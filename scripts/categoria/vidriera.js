f94ff01ff466ac24f06db9bf4749bcc6bb9025a2
Francisco González <frgonzalez.415@gmail.com>
2024-05-20 16:52:07 -0300

    se modifican nombres de carpetas e imagenes

--- scripts/categoria/vidriera.js
+++ scripts/categoria/vidriera.js
@@ -7,6 +7,7 @@ const juegos = document.querySelectorAll( 'main .vidriera .juego' )
 juegos.forEach( ( juego, i ) => {
     let nombreDelJuego = juegos[i].querySelector( '.nombre' ).textContent.toLowerCase().replace( /[.]/g, '' ).replace( /[ ]/g, '_' )
     console.log(nombreDelJuego)
+    console.log(nombreDelJuego)
     juegos[i].style.backgroundImage = `url(../imagenes/juegos/${nombreDelJuego}.png)`
     })
 
