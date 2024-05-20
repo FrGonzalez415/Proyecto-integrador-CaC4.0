e3cee30f9fa4e33d0b1d45271ee498e8550ed585
Francisco González <frgonzalez.415@gmail.com>
2024-05-20 16:54:40 -0300

    se modifica nombre de ruta

--- scripts/categoria/vidriera.js
+++ scripts/categoria/vidriera.js
@@ -8,6 +8,6 @@ juegos.forEach( ( juego, i ) => {
     let nombreDelJuego = juegos[i].querySelector( '.nombre' ).textContent.toLowerCase().replace( /[.]/g, '' ).replace( /[ ]/g, '_' )
     console.log(nombreDelJuego)
     console.log(nombreDelJuego)
-    juegos[i].style.backgroundImage = `url(../imagenes/juegos/${nombreDelJuego}.png)`
+    juegos[i].style.backgroundImage = `url(../imagenes/Juegos/${nombreDelJuego}.png)`
     })
 
