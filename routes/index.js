a7aefe2449da53738e99aa110f82aa534b8850b9
Francisco González <frgonzalez.415@gmail.com>
2024-07-06 18:06:31 -0300

    Se corrigen inconsistencias en rutas a imagenes y vistas

--- routes/index.js
+++ routes/index.js
@@ -20,62 +20,67 @@ router.get('/', (req, res) => {
 //                                  CATEGORIAS
 // Cartas
 router.get('/cartas', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 // Clásicos
 router.get('/clasicos', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 // Cooperativos
 router.get('/cooperativos', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 // Estrategia
 router.get('/estrategia', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 // Familiares
 router.get('/familiares', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 // Ofertas
 router.get('/ofertas', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 //                                  REGISTRO
 router.get('/registro', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 //                                  LOGIN 
 router.get('/login', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 //                                  CONTACTO
 router.get('/contacto', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 //                                  NOSOTROS
 router.get('/nosotros', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 //                                  TERMINOS Y CONDICIONES
 router.get('/terminos-y-condiciones', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 //                                  ARREPENTIMIENTO DE COMPRA
 router.get('/arrepentimiento', (req, res) => {
-    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url + '.html'))
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
+})
+
+//                                  CARRITO
+router.get('/carrito', (req, res) => {
+    res.type('html').sendFile(path.join(__dirname, '..', 'public', 'HTML', req.url.toLowerCase() + '.html'))
 })
 
 
